35e65fb037123d64b8a7f4f26a00fef360d3a03f
Seth Steinbrook <steinbrs@mail.gvsu.edu>
2022-10-30 22:34:33 -0400

    Adds base logic for packet types

--- header.hpp
+++ header.hpp
@@ -25,4 +25,17 @@ typedef struct ARPPacket{
     ARPHeader ARP_header;
     EtherHeader ether_header;
 }ARPPacket;
+
+typedef struct ICMPHeader{
+	uint8_t type;
+	uint8_t code;
+	uint16_t checksum;
+	uint8_t identifier;
+	uint8_t seq_num;
+	char data[1500];
+}ICMPHeader;
+
+void createIcmpReply(struct EtherHeader, struct IPHeader, struct ICMPHeader);
+void createArpReply(struct EtherHeader, struct ARPHeader);
+
 #endif
--- router.cpp
+++ router.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <unistd.h>
 #include <sys/select.h>
 #include <netpacket/packet.h> 
 #include <net/ethernet.h>
+#include <net/if.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <ifaddrs.h>
@@ -17,58 +20,84 @@
 using namespace std;
 
 /* struct fd_set {
-  int fd_count;
-  socket fd_array[];
+	int fd_count;
+	socket fd_array[];
 }
 */
 
 int main(int argc, char **argv){
-  int packet_sockets[16];
-  struct ifaddr *ifaddr, *tmp;
+	int packet_sockets[16];
+	struct ifaddrs *ifaddr, *tmp;
 
-  if(getifaddrs(&ifaddr)==-1){
-    perror("getifaddrs");
-    return 1;
-  }
+	if(getifaddrs(&ifaddr)==-1){
+		perror("getifaddrs");
+		return 1;
+	}
 
-  int i = 0;
+	int i = 0;
 
-  // Puts all packet sockets into array
-  for(tmp = ifaddr; tmp!=NULL; tmp=tmp->ifa_next){
-    if(tmp->ifa_addr->sa_family==AF_PACKET){
-	  cout << "Interface: " << tmp->ifa_name << endl;
+	// Puts all packet sockets into array
+	for(tmp = ifaddr; tmp!=NULL; tmp=tmp->ifa_next){
+		if(tmp->ifa_addr->sa_family==AF_PACKET){
+		cout << "Interface: " << tmp->ifa_name << endl;
 
-      if(!strncmp(&(tmp->ifa_name[3]),"eth",3)){
-        cout << "Creating Socket on interface " << tmp->ifa_name << endl;
+			if(!strncmp(&(tmp->ifa_name[3]),"eth",3)){
+				cout << "Creating Socket on interface " << tmp->ifa_name << endl;
 
-        packet_socket[i] = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-        if(packet_socket[i]<0){
-          perror("socket");
-          return 2;
-        }
+				packet_socket[i] = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+				if(packet_socket[i]<0){
+					perror("socket");
+					return 2;
+				}
 
-        if(bind(packet_socket[i],tmp->ifa_addr,sizeof(struct sockaddr_ll))==-1){
-          perror("bind");
-        }
+				if(bind(packet_socket[i],tmp->ifa_addr,sizeof(struct sockaddr_ll))==-1){
+					perror("bind");
+				}
 
-        i++;
-      }
-    }
-  }
+				i++;
+			}
+		}
+	}
 
-  // Creates a fd_set with all the sockets
-  fd_set fds;
+	// Creates a fd_set with all the sockets
+	fd_set fds;
 
-  FD_ZERO(&fds);
+	FD_ZERO(&fds);
 
-  for(int j=0;j<i;j++){
-    FD_SET(packet_sockets[j], &fds);
-  }
+	for(int j=0;j<i;j++){
+		FD_SET(packet_sockets[j], &fds);
+	}
 
-  while(1){
-    fd_set tmp = fds;
-    int nn=select(FD_SETSIZE, &tmp, NULL, NULL, NULL);
-  }
+	while(1){
+		fd_set tmp = fds;
+		int nn=select(FD_SETSIZE, &tmp, NULL, NULL, NULL);
+		if(FD_ISSET(sockfd,&tmp)){
+			cout << "Got something on the socket" << endl;
+			socklen_t len = sizeof(clientaddr);
+			char line[5000];
+			int n = recvfrom(sockfd,line,5000,0,
+					 (struct sockaddr*)&clientaddr,&len);
 
+			struct ether_header eh;
+			memcpy(&eh, line, 14);
 
+			// ip type is 0x0800, for pings
+			if(ntohs(eh.ether_type) == ETHERTYPE_IP) {
+				memcpy(&eh,line,14);
+				struct iphdr iph;
+				struct ICMPHeader icmph;
+				memcpy(&iph,line+14,20);
+
+				// ICMP echo reply consists of the ethernet header, ip header, and icmp header
+				createIcmpReply(eh, iph, icmph);
+			}
+			// arp type is 0x0806
+			if(ntohs(eh.ether_type) == ETHERTYPE_ARP) {
+				struct arphdr arph;
+				memcpy(&arph, line+14, 20);
+				// ARP reply consists of the ethernet header and ARP header
+				createArpReply(eh, arph);
+			}
+		}
+	}
 }
