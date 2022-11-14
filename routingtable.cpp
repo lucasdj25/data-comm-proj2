43e7e3482cdd40dd9984253b16faec6b4682be3c
lucasdj25 <lucasjmax@gmail.com>
2022-11-13 22:41:50 -0500

    Fixes routing table lookup

--- routingtable.cpp
+++ routingtable.cpp
@@ -8,10 +8,9 @@ std::string getRouterIP(struct routingTableRow table[], int tableLen, std::strin
     std::string subDestIP = destIP.substr(0,6);
     for(int i = 0; i < tableLen; i++) {
         std::string subRouterIP = table[i].networkPrefix.substr(0,6);
-        if(subRouterIP.compare(subDestIP) == 0) {
 			int dC1, dC2, dC3, dC4, rC1, rC2, rC3, rC4, range;
 			sscanf(destIP.c_str(), "%d.%d.%d.%d", &dC1, &dC2, &dC3, &dC4);
-			sscanf(table[i].networkPrefix.c_str(), "%d.%d.%d.%d/%d", &rC1, &rC2, &rC3, &rC4, &range);
+			sscanf(table[i].networkPrefix.c_str(), "%d.%d.%d.%d/%d", &rC1, &rC2, &rC3, &rC4, &range);  
 			if(range == 16) {
 				rC3 = 255;
 				rC4 = 255;
@@ -21,7 +20,7 @@ std::string getRouterIP(struct routingTableRow table[], int tableLen, std::strin
 			}
 			
 			// destIP is in range of the router ips
-			if(dC3 <= rC3 && dC4 <= rC4) {
+			if(dC2 == rC2 && dC3 <= rC3 && dC4 <= rC4) {
 				if(table[i].nextHopDevice.length() > 1) {
 					routerIP = table[i].nextHopDevice;
 					std::cout << "Packet needs to hop to device: " << routerIP << std::endl;
@@ -34,12 +33,7 @@ std::string getRouterIP(struct routingTableRow table[], int tableLen, std::strin
 			}
 			else {
 				routerIP = "DNE";
-				break;
 			}
-        }
-		else {
-			routerIP = "DNE";
-		}
     }
     return routerIP;
 }
